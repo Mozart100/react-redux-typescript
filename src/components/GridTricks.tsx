2b99e25e89ca0c96b27dab3bfe4eab334a407a53
Anatoliy Ark <anatoliy.arkove@gmail.com>
2017-11-23 22:15:13 +0200

    Working GrifdTricks

--- src/components/GridTricks.tsx
+++ src/components/GridTricks.tsx
@@ -16,22 +16,25 @@ const RectangleComp = props =>
   )
 
 export class GridTricks extends React.Component<GridTricksProps, GridTricksState> {
-
+ private _rows : number = 16;
+ private _columns : number = 16;
+ 
+ //-------------------------------------------------------------------------------------------------------
+ //-------------------------------------------------------------------------------------------------------
+ 
   constructor(props) {
     super(props);
 
-    let mtrx: number[][] = [];
+    const mtrx: number[][] = [];
+    let columnPosition: number = 0;
 
-    const rows: number = 5;
-    const columns: number = 16;
-    const columnPosition: number = 0;
+    for (let x = 0; x < this._rows; x++) {
 
-    for (let x = 0; x < rows; x++) {
-
-      mtrx[x] = new Array(columns);
-      for (let y = 0; y < columns; y++) {
+      mtrx[x] = new Array(this._columns);
+      for (let y = 0; y < this._columns; y++) {
         mtrx[x][y] = y === columnPosition ? 1 : 0;
       }
+      columnPosition++;
     }
 
     this.state = { positions: mtrx };
@@ -40,70 +43,30 @@ export class GridTricks extends React.Component<GridTricksProps, GridTricksState
   //-------------------------------------------------------------------------------------------------------
   //-------------------------------------------------------------------------------------------------------
 
-
-  private createColumn = (column: number, index: number) => {
-
+  private createColumn = (column: number, index: string) => {
     const rectangle = column > 0 ? <RectangleComp /> : null;
-
     return (
-    <Grid.Column index={index}>
-      {rectangle}
-    </Grid.Column>)
+      <Grid.Column index={index}>
+        {rectangle}
+      </Grid.Column>)
   }
 
 
   private createRows = (row: number[], index: number) => {
     return (
       <Grid.Row columns={16} index={index}>
-        {row.map(this.createColumn)}
+        {row.map((x, newindex) => {
+          return this.createColumn(x, index + "." + newindex)
+        })}
       </Grid.Row>
-    )
-
-  }
-
-  private createMtrx = (metrix: number[], index: number) => {
-    
-    return (
-      <Grid>
-        {/* {metrix.map(this.createRows)} */}
-      </Grid>
-      
     );
-    // var list : any;
-    // for (;let x: number = 0; x < metrix.length; x++) {
-    //   this.createRows(metrix[x], index);
-    //     list.push(this.createRows(metrix[x], index))
-    // }
 
-    // return list;
   }
 
   render() {
     return (
       <Grid >
-        {this.state.positions.map(this.createMtrx)}
-        {/* <Grid.Row columns={16}>
-          <Grid.Column>
-            <RectangleComp />
-          </Grid.Column>
-          <Grid.Column>
-          </Grid.Column>
-          <Grid.Column>
-          </Grid.Column>
-        </Grid.Row>
-
-        <Grid.Row columns={16}>
-          <Grid.Column>
-          </Grid.Column>
-
-          <Grid.Column >
-            <RectangleComp />
-          </Grid.Column>
-
-          <Grid.Column>
-          </Grid.Column>
-
-        </Grid.Row> */}
+        {this.state.positions.map(this.createRows)}
       </Grid>
     );
   }
